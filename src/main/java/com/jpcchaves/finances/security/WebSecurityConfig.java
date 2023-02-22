04971c1c73f91f33b8ae0d0a1c015ec7b4a3973c
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-22 14:00:03 -0300

    feat: add web security configuration

---
+++ src/main/java/com/jpcchaves/finances/security/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.jpcchaves.finances.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests((auth) ->
+                        auth.requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilter(new JwtAuthenticationFilter(getAuthenticationManager(authenticationConfiguration), jwtUtil));
+        http.addFilter(new JwtAuthorizationFilter(getAuthenticationManager(authenticationConfiguration), jwtUtil));
+
+        return http.build();
+    }
+
+}
