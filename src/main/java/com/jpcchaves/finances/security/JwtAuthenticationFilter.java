06baf72e355eff1b7a0ace2f42daa194a4df3dce
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-22 13:05:47 -0300

    feat: add unsuccessful auth handler

--- src/main/java/com/jpcchaves/finances/security/JwtAuthenticationFilter.java
+++ src/main/java/com/jpcchaves/finances/security/JwtAuthenticationFilter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.jpcchaves.finances.common.DataConverter;
+import com.jpcchaves.finances.domain.model.ErrorResponse;
 import com.jpcchaves.finances.domain.model.User;
 import com.jpcchaves.finances.dto.user.LoginRequestDto;
 import com.jpcchaves.finances.dto.user.LoginResponseDto;
@@ -14,6 +16,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
@@ -23,6 +27,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -74,5 +79,17 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.getWriter().write(new Gson().toJson(loginResponseDto));
     }
 
-   
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+        String timestamp = DataConverter.convertDateToString(new Date());
+        ErrorResponse errorResponse = new ErrorResponse(timestamp, HttpStatus.UNAUTHORIZED.value(), "Unauthorized", failed.getMessage());
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        response.getWriter().write(new Gson().toJson(errorResponse));
+    }
 }
