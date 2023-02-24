3fdf6211d10c75f100cccae9d76a951c40934afd
jpcchaves <joaopaulodecastrochaves@hotmail.com>
2023-02-24 09:41:46 -0300

    feat: add title repository and dtos

--- src/main/java/com/jpcchaves/finances/domain/repository/TitleRepository.java
+++ src/main/java/com/jpcchaves/finances/domain/repository/TitleRepository.java
@@ -1,9 +1,9 @@
 package com.jpcchaves.finances.domain.repository;
 
-import com.jpcchaves.finances.domain.model.CostCenter;
+import com.jpcchaves.finances.domain.model.Title;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CostCenterRepository extends JpaRepository<CostCenter, Long> {
+public interface TitleRepository extends JpaRepository<Title, Long> {
 }
---
+++ src/main/java/com/jpcchaves/finances/dto/title/TitleResponseDto.java
@@ -0,0 +1,106 @@
+package com.jpcchaves.finances.dto.title;
+
+import com.jpcchaves.finances.domain.Enum.ETitleType;
+import com.jpcchaves.finances.dto.costcenter.CostCenterRequestDto;
+import com.jpcchaves.finances.dto.costcenter.CostCenterResponseDto;
+
+import java.util.Date;
+import java.util.List;
+
+public class TitleResponseDto {
+    private Long id;
+    private String description;
+
+    private ETitleType type;
+    private List<CostCenterResponseDto> costcenter;
+
+    private Double amount;
+    private Date createdAt;
+
+    private Date referenceDate;
+
+    private Date expirationDate;
+
+    private Date paymentDate;
+    private String note;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ETitleType getType() {
+        return type;
+    }
+
+    public void setType(ETitleType type) {
+        this.type = type;
+    }
+
+    public List<CostCenterResponseDto> getCostcenter() {
+        return costcenter;
+    }
+
+    public void setCostcenter(List<CostCenterResponseDto> costcenter) {
+        this.costcenter = costcenter;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getReferenceDate() {
+        return referenceDate;
+    }
+
+    public void setReferenceDate(Date referenceDate) {
+        this.referenceDate = referenceDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
