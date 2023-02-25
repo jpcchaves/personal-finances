c72646c069c0c4a6024659cb9e82b31eba5d2182
João Paulo <jpcchaves@outlook.com>
2023-02-25 11:25:27 -0300

    feat: add title swagger docs

--- src/main/java/com/jpcchaves/finances/controller/TitleController.java
+++ src/main/java/com/jpcchaves/finances/controller/TitleController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,7 @@ public class TitleController {
     @Autowired
     private TitleService titleService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Finds all titles", description = "Finds all titles related to the current logged user",
             tags = {"Titles"},
             responses = {
@@ -46,7 +47,7 @@ public class TitleController {
                 .ok(titleService.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Finds a title", description = "Finds a title related to the current logged user",
             tags = {"Titles"},
             responses = {
@@ -68,21 +69,69 @@ public class TitleController {
                 .ok(titleService.findById(id));
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Creates a title", description = "Creates a title related to the current logged user",
+            tags = {"Titles"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TitleResponseDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<TitleResponseDto> create(@Valid @RequestBody TitleRequestDto dto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(titleService.create(dto));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Updates a title", description = "Updates a title related to the current logged user",
+            tags = {"Titles"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TitleResponseDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<TitleResponseDto> update(@PathVariable Long id, @Valid @RequestBody TitleRequestDto dto) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(titleService.update(id, dto));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Deletes a title by passing his ID", description = "Deletes a title related to the current logged user by passing the Title ID",
+            tags = {"Titles"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TitleResponseDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<?> delete(@PathVariable Long id) {
         titleService.delete(id);
         return ResponseEntity.noContent().build();
